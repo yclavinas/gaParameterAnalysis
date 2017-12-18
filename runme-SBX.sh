995e114a01f1b1cc2a62a565397b603ee21011bb
Yuri Lavinas <yurilavinas@Yuris-MacBook-Pro.local>
2017-12-18 16:42:26 +0900

    runme sbx: testing

--- runme-SBX.sh
+++ runme-SBX.sh
@@ -3,7 +3,7 @@ do
 	# repetition loop
 	for j in {0..1} 
 	do
-		nohup python2.7 gaBenchmarksStudy/benchmarks/-SBXbenchmarks-gamodelF9.py -tournsize "$i" -params 'benchmarks/gaParams.txt' & 
+		nohup python2.7 gaBenchmarksStudy/benchmarks-SBX/benchmarks-gamodelF9.py -tournsize "$i" -params 'benchmarks/gaParams.txt' & 
 		# nohup python2.7 benchmarks-pseudo-adaptative/benchmarks-gamodelF10.py -tournsize "$i" -params 'benchmarks-pseudo-adaptative/gaParams.txt' &
 		# nohup python2.7 benchmarks-pseudo-adaptative/benchmarks-gamodelF11.py -tournsize "$i" -params 'benchmarks-pseudo-adaptative/gaParams.txt' &
 		# nohup python2.7 benchmarks-pseudo-adaptative/benchmarks-gamodelF12.py -tournsize "$i" -params 'benchmarks-pseudo-adaptative/gaParams.txt' &
