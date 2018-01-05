d9b4aaf115f60a652d13ef0e0f608227e9ace74e
Yuri Lavinas <yurilavinas@Yuris-MacBook-Pro.local>
2018-01-05 11:43:33 +0900

    testing 2n

--- runme-uniform.sh
+++ runme-uniform.sh
@@ -3,7 +3,7 @@ do
 	# repetition loop
 	for j in {0..40} 
 	do
-		# nohup python2.7 gaBenchmarksStudy/benchmarks-uniform/benchmarks-gamodelF1.py -tournsize "$i" -params 'gaBenchmarksStudy/benchmarks-uniform/gaParams.txt' & 
+		nohup python2.7 gaBenchmarksStudy/benchmarks-uniform/benchmarks-gamodelF1.py -tournsize "$i" -params 'gaBenchmarksStudy/benchmarks-uniform/gaParams.txt' & 
 		nohup python2.7 gaBenchmarksStudy/benchmarks-uniform/benchmarks-gamodelF2.py -tournsize "$i" -params 'gaBenchmarksStudy/benchmarks-uniform/gaParams.txt' & 
 		nohup python2.7 gaBenchmarksStudy/benchmarks-uniform/benchmarks-gamodelF3.py -tournsize "$i" -params 'gaBenchmarksStudy/benchmarks-uniform/gaParams.txt' & 
 		nohup python2.7 gaBenchmarksStudy/benchmarks-uniform/benchmarks-gamodelF4.py -tournsize "$i" -params 'gaBenchmarksStudy/benchmarks-uniform/gaParams.txt' & 
