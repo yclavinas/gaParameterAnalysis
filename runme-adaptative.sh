ec539f1379df9157fb0c379b3dba8809de2fef5f
Yuri Lavinas <yurilavinas@Yuris-MacBook-Pro.local>
2018-01-09 11:53:14 +0900

    adaptative

--- runme-adaptative.sh
+++ runme-adaptative.sh
@@ -3,32 +3,32 @@ do
 	# repetition loop
 	for j in {0..40} 
 	do
-		nohup python2.7 gaBenchmarksStudy/benchmarks-pseudo-adaptative/benchmarks-gamodelF1.py -tournsize "$i" -params 'gaBenchmarksStudy/benchmarks-pseudo-adaptative/gaParams.txt' & 
-		nohup python2.7 gaBenchmarksStudy/benchmarks-pseudo-adaptative/benchmarks-gamodelF2.py -tournsize "$i" -params 'gaBenchmarksStudy/benchmarks-pseudo-adaptative/gaParams.txt' & 
-		nohup python2.7 gaBenchmarksStudy/benchmarks-pseudo-adaptative/benchmarks-gamodelF3.py -tournsize "$i" -params 'gaBenchmarksStudy/benchmarks-pseudo-adaptative/gaParams.txt' & 
-		nohup python2.7 gaBenchmarksStudy/benchmarks-pseudo-adaptative/benchmarks-gamodelF4.py -tournsize "$i" -params 'gaBenchmarksStudy/benchmarks-pseudo-adaptative/gaParams.txt' & 
-		nohup python2.7 gaBenchmarksStudy/benchmarks-pseudo-adaptative/benchmarks-gamodelF5.py -tournsize "$i" -params 'gaBenchmarksStudy/benchmarks-pseudo-adaptative/gaParams.txt' & 
-		nohup python2.7 gaBenchmarksStudy/benchmarks-pseudo-adaptative/benchmarks-gamodelF6.py -tournsize "$i" -params 'gaBenchmarksStudy/benchmarks-pseudo-adaptative/gaParams.txt' & 
-		nohup python2.7 gaBenchmarksStudy/benchmarks-pseudo-adaptative/benchmarks-gamodelF7.py -tournsize "$i" -params 'gaBenchmarksStudy/benchmarks-pseudo-adaptative/gaParams.txt' & 
-		nohup python2.7 gaBenchmarksStudy/benchmarks-pseudo-adaptative/benchmarks-gamodelF8.py -tournsize "$i" -params 'gaBenchmarksStudy/benchmarks-pseudo-adaptative/gaParams.txt' & 
-		nohup python2.7 gaBenchmarksStudy/benchmarks-pseudo-adaptative/benchmarks-gamodelF9.py -tournsize "$i" -params 'gaBenchmarksStudy/benchmarks-pseudo-adaptative/gaParams.txt' & 
-		nohup python2.7 gaBenchmarksStudy/benchmarks-pseudo-adaptative/benchmarks-gamodelF10.py -tournsize "$i" -params 'gaBenchmarksStudy/benchmarks-pseudo-adaptative/gaParams.txt' &
+		nohup python2.7 gaBenchmarksStudy/benchmarks-pseudo-adaptative-sbx/benchmarks-gamodelF1.py -tournsize "$i" -params 'gaBenchmarksStudy/benchmarks-pseudo-adaptative-sbx/gaParams.txt' & 
+		nohup python2.7 gaBenchmarksStudy/benchmarks-pseudo-adaptative-sbx/benchmarks-gamodelF2.py -tournsize "$i" -params 'gaBenchmarksStudy/benchmarks-pseudo-adaptative-sbx/gaParams.txt' & 
+		nohup python2.7 gaBenchmarksStudy/benchmarks-pseudo-adaptative-sbx/benchmarks-gamodelF3.py -tournsize "$i" -params 'gaBenchmarksStudy/benchmarks-pseudo-adaptative-sbx/gaParams.txt' & 
+		nohup python2.7 gaBenchmarksStudy/benchmarks-pseudo-adaptative-sbx/benchmarks-gamodelF4.py -tournsize "$i" -params 'gaBenchmarksStudy/benchmarks-pseudo-adaptative-sbx/gaParams.txt' & 
+		nohup python2.7 gaBenchmarksStudy/benchmarks-pseudo-adaptative-sbx/benchmarks-gamodelF5.py -tournsize "$i" -params 'gaBenchmarksStudy/benchmarks-pseudo-adaptative-sbx/gaParams.txt' & 
+		nohup python2.7 gaBenchmarksStudy/benchmarks-pseudo-adaptative-sbx/benchmarks-gamodelF6.py -tournsize "$i" -params 'gaBenchmarksStudy/benchmarks-pseudo-adaptative-sbx/gaParams.txt' & 
+		nohup python2.7 gaBenchmarksStudy/benchmarks-pseudo-adaptative-sbx/benchmarks-gamodelF7.py -tournsize "$i" -params 'gaBenchmarksStudy/benchmarks-pseudo-adaptative-sbx/gaParams.txt' & 
+		nohup python2.7 gaBenchmarksStudy/benchmarks-pseudo-adaptative-sbx/benchmarks-gamodelF8.py -tournsize "$i" -params 'gaBenchmarksStudy/benchmarks-pseudo-adaptative-sbx/gaParams.txt' & 
+		nohup python2.7 gaBenchmarksStudy/benchmarks-pseudo-adaptative-sbx/benchmarks-gamodelF9.py -tournsize "$i" -params 'gaBenchmarksStudy/benchmarks-pseudo-adaptative-sbx/gaParams.txt' & 
+		nohup python2.7 gaBenchmarksStudy/benchmarks-pseudo-adaptative-sbx/benchmarks-gamodelF10.py -tournsize "$i" -params 'gaBenchmarksStudy/benchmarks-pseudo-adaptative-sbx/gaParams.txt' &
 		wait 
-		nohup python2.7 gaBenchmarksStudy/benchmarks-pseudo-adaptative/benchmarks-gamodelF11.py -tournsize "$i" -params 'gaBenchmarksStudy/benchmarks-pseudo-adaptative/gaParams.txt' & 
-		nohup python2.7 gaBenchmarksStudy/benchmarks-pseudo-adaptative/benchmarks-gamodelF12.py -tournsize "$i" -params 'gaBenchmarksStudy/benchmarks-pseudo-adaptative/gaParams.txt' & 
-		nohup python2.7 gaBenchmarksStudy/benchmarks-pseudo-adaptative/benchmarks-gamodelF13.py -tournsize "$i" -params 'gaBenchmarksStudy/benchmarks-pseudo-adaptative/gaParams.txt' & 
-		nohup python2.7 gaBenchmarksStudy/benchmarks-pseudo-adaptative/benchmarks-gamodelF14.py -tournsize "$i" -params 'gaBenchmarksStudy/benchmarks-pseudo-adaptative/gaParams.txt' & 
-		nohup python2.7 gaBenchmarksStudy/benchmarks-pseudo-adaptative/benchmarks-gamodelF15.py -tournsize "$i" -params 'gaBenchmarksStudy/benchmarks-pseudo-adaptative/gaParams.txt' & 
-		nohup python2.7 gaBenchmarksStudy/benchmarks-pseudo-adaptative/benchmarks-gamodelF16.py -tournsize "$i" -params 'gaBenchmarksStudy/benchmarks-pseudo-adaptative/gaParams.txt' & 
-		nohup python2.7 gaBenchmarksStudy/benchmarks-pseudo-adaptative/benchmarks-gamodelF17.py -tournsize "$i" -params 'gaBenchmarksStudy/benchmarks-pseudo-adaptative/gaParams.txt' & 
-		nohup python2.7 gaBenchmarksStudy/benchmarks-pseudo-adaptative/benchmarks-gamodelF18.py -tournsize "$i" -params 'gaBenchmarksStudy/benchmarks-pseudo-adaptative/gaParams.txt' & 
-		nohup python2.7 gaBenchmarksStudy/benchmarks-pseudo-adaptative/benchmarks-gamodelF19.py -tournsize "$i" -params 'gaBenchmarksStudy/benchmarks-pseudo-adaptative/gaParams.txt' & 
-		nohup python2.7 gaBenchmarksStudy/benchmarks-pseudo-adaptative/benchmarks-gamodelF20.py -tournsize "$i" -params 'gaBenchmarksStudy/benchmarks-pseudo-adaptative/gaParams.txt' & 
+		nohup python2.7 gaBenchmarksStudy/benchmarks-pseudo-adaptative-sbx/benchmarks-gamodelF11.py -tournsize "$i" -params 'gaBenchmarksStudy/benchmarks-pseudo-adaptative-sbx/gaParams.txt' & 
+		nohup python2.7 gaBenchmarksStudy/benchmarks-pseudo-adaptative-sbx/benchmarks-gamodelF12.py -tournsize "$i" -params 'gaBenchmarksStudy/benchmarks-pseudo-adaptative-sbx/gaParams.txt' & 
+		nohup python2.7 gaBenchmarksStudy/benchmarks-pseudo-adaptative-sbx/benchmarks-gamodelF13.py -tournsize "$i" -params 'gaBenchmarksStudy/benchmarks-pseudo-adaptative-sbx/gaParams.txt' & 
+		nohup python2.7 gaBenchmarksStudy/benchmarks-pseudo-adaptative-sbx/benchmarks-gamodelF14.py -tournsize "$i" -params 'gaBenchmarksStudy/benchmarks-pseudo-adaptative-sbx/gaParams.txt' & 
+		nohup python2.7 gaBenchmarksStudy/benchmarks-pseudo-adaptative-sbx/benchmarks-gamodelF15.py -tournsize "$i" -params 'gaBenchmarksStudy/benchmarks-pseudo-adaptative-sbx/gaParams.txt' & 
+		nohup python2.7 gaBenchmarksStudy/benchmarks-pseudo-adaptative-sbx/benchmarks-gamodelF16.py -tournsize "$i" -params 'gaBenchmarksStudy/benchmarks-pseudo-adaptative-sbx/gaParams.txt' & 
+		nohup python2.7 gaBenchmarksStudy/benchmarks-pseudo-adaptative-sbx/benchmarks-gamodelF17.py -tournsize "$i" -params 'gaBenchmarksStudy/benchmarks-pseudo-adaptative-sbx/gaParams.txt' & 
+		nohup python2.7 gaBenchmarksStudy/benchmarks-pseudo-adaptative-sbx/benchmarks-gamodelF18.py -tournsize "$i" -params 'gaBenchmarksStudy/benchmarks-pseudo-adaptative-sbx/gaParams.txt' & 
+		nohup python2.7 gaBenchmarksStudy/benchmarks-pseudo-adaptative-sbx/benchmarks-gamodelF19.py -tournsize "$i" -params 'gaBenchmarksStudy/benchmarks-pseudo-adaptative-sbx/gaParams.txt' & 
+		nohup python2.7 gaBenchmarksStudy/benchmarks-pseudo-adaptative-sbx/benchmarks-gamodelF20.py -tournsize "$i" -params 'gaBenchmarksStudy/benchmarks-pseudo-adaptative-sbx/gaParams.txt' & 
 		wait
-		nohup python2.7 gaBenchmarksStudy/benchmarks-pseudo-adaptative/benchmarks-gamodelF21.py -tournsize "$i" -params 'gaBenchmarksStudy/benchmarks-pseudo-adaptative/gaParams.txt' & 
-		nohup python2.7 gaBenchmarksStudy/benchmarks-pseudo-adaptative/benchmarks-gamodelF22.py -tournsize "$i" -params 'gaBenchmarksStudy/benchmarks-pseudo-adaptative/gaParams.txt' & 
-		nohup python2.7 gaBenchmarksStudy/benchmarks-pseudo-adaptative/benchmarks-gamodelF23.py -tournsize "$i" -params 'gaBenchmarksStudy/benchmarks-pseudo-adaptative/gaParams.txt' & 
-		nohup python2.7 gaBenchmarksStudy/benchmarks-pseudo-adaptative/benchmarks-gamodelF24.py -tournsize "$i" -params 'gaBenchmarksStudy/benchmarks-pseudo-adaptative/gaParams.txt' & 
+		nohup python2.7 gaBenchmarksStudy/benchmarks-pseudo-adaptative-sbx/benchmarks-gamodelF21.py -tournsize "$i" -params 'gaBenchmarksStudy/benchmarks-pseudo-adaptative-sbx/gaParams.txt' & 
+		nohup python2.7 gaBenchmarksStudy/benchmarks-pseudo-adaptative-sbx/benchmarks-gamodelF22.py -tournsize "$i" -params 'gaBenchmarksStudy/benchmarks-pseudo-adaptative-sbx/gaParams.txt' & 
+		nohup python2.7 gaBenchmarksStudy/benchmarks-pseudo-adaptative-sbx/benchmarks-gamodelF23.py -tournsize "$i" -params 'gaBenchmarksStudy/benchmarks-pseudo-adaptative-sbx/gaParams.txt' & 
+		nohup python2.7 gaBenchmarksStudy/benchmarks-pseudo-adaptative-sbx/benchmarks-gamodelF24.py -tournsize "$i" -params 'gaBenchmarksStudy/benchmarks-pseudo-adaptative-sbx/gaParams.txt' & 
 		wait
 	done
 done
