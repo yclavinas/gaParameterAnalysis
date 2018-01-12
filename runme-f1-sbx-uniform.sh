0dd6601277a6f8070bc620f6645b73cdc08c4540
Yuri Lavinas <yurilavinas@Yuris-MacBook-Pro.local>
2018-01-12 18:22:09 +0900

    f1: exp, it was missing

---
+++ runme-f1-sbx-uniform.sh
@@ -0,0 +1,10 @@
+for i in {2..2} 
+do
+	# repetition loop
+	for j in {0..40} 
+	do
+		nohup python2.7 gaBenchmarksStudy/benchmarks-SBX/benchmarks-gamodelF1.py -tournsize "$i" -params 'gaBenchmarksStudy/benchmarks-SBX/gaParams.txt' & 
+		nohup python2.7 gaBenchmarksStudy/benchmarks-uniform/benchmarks-gamodelF1.py -tournsize "$i" -params 'gaBenchmarksStudy/benchmarks-uniform/gaParams.txt' & 
+		wait
+	done
+done
