3c30ebf47641ebbfb380c1562860f37a211846cd
Yuri Lavinas <yurilavinas@Yuris-MacBook-Pro.local>
2018-01-15 09:45:37 +0900

    exp: most of missing exp were done

---
+++ runme-missing-10.sh
@@ -0,0 +1,30 @@
+nohup python2.7 gaBenchmarksStudy/benchmarks-uniform/benchmarks-gamodelF1.py -tournsize 14 -params 'gaBenchmarksStudy/benchmarks-uniform/gaParams.txt' & 
+nohup python2.7 gaBenchmarksStudy/benchmarks-uniform/benchmarks-gamodelF2.py -tournsize 14 -params 'gaBenchmarksStudy/benchmarks-uniform/gaParams.txt' & 
+nohup python2.7 gaBenchmarksStudy/benchmarks-uniform/benchmarks-gamodelF3.py -tournsize 14 -params 'gaBenchmarksStudy/benchmarks-uniform/gaParams.txt' & 
+nohup python2.7 gaBenchmarksStudy/benchmarks-uniform/benchmarks-gamodelF4.py -tournsize 14 -params 'gaBenchmarksStudy/benchmarks-uniform/gaParams.txt' & 
+wait
+nohup python2.7 gaBenchmarksStudy/benchmarks-uniform/benchmarks-gamodelF5.py -tournsize 14 -params 'gaBenchmarksStudy/benchmarks-uniform/gaParams.txt' & 
+nohup python2.7 gaBenchmarksStudy/benchmarks-uniform/benchmarks-gamodelF6.py -tournsize 14 -params 'gaBenchmarksStudy/benchmarks-uniform/gaParams.txt' & 
+nohup python2.7 gaBenchmarksStudy/benchmarks-uniform/benchmarks-gamodelF7.py -tournsize 14 -params 'gaBenchmarksStudy/benchmarks-uniform/gaParams.txt' & 
+nohup python2.7 gaBenchmarksStudy/benchmarks-uniform/benchmarks-gamodelF8.py -tournsize 14 -params 'gaBenchmarksStudy/benchmarks-uniform/gaParams.txt' & 
+wait
+nohup python2.7 gaBenchmarksStudy/benchmarks-uniform/benchmarks-gamodelF9.py -tournsize 14 -params 'gaBenchmarksStudy/benchmarks-uniform/gaParams.txt' & 
+nohup python2.7 gaBenchmarksStudy/benchmarks-uniform/benchmarks-gamodelF10.py -tournsize 14 -params 'gaBenchmarksStudy/benchmarks-uniform/gaParams.txt' & 
+nohup python2.7 gaBenchmarksStudy/benchmarks-uniform/benchmarks-gamodelF11.py -tournsize 14 -params 'gaBenchmarksStudy/benchmarks-uniform/gaParams.txt' & 
+nohup python2.7 gaBenchmarksStudy/benchmarks-uniform/benchmarks-gamodelF12.py -tournsize 14 -params 'gaBenchmarksStudy/benchmarks-uniform/gaParams.txt' & 
+wait
+nohup python2.7 gaBenchmarksStudy/benchmarks-uniform/benchmarks-gamodelF13.py -tournsize 14 -params 'gaBenchmarksStudy/benchmarks-uniform/gaParams.txt' & 
+nohup python2.7 gaBenchmarksStudy/benchmarks-uniform/benchmarks-gamodelF14.py -tournsize 14 -params 'gaBenchmarksStudy/benchmarks-uniform/gaParams.txt' & 
+nohup python2.7 gaBenchmarksStudy/benchmarks-uniform/benchmarks-gamodelF15.py -tournsize 14 -params 'gaBenchmarksStudy/benchmarks-uniform/gaParams.txt' & 
+nohup python2.7 gaBenchmarksStudy/benchmarks-uniform/benchmarks-gamodelF16.py -tournsize 14 -params 'gaBenchmarksStudy/benchmarks-uniform/gaParams.txt' & 
+wait
+nohup python2.7 gaBenchmarksStudy/benchmarks-uniform/benchmarks-gamodelF17.py -tournsize 14 -params 'gaBenchmarksStudy/benchmarks-uniform/gaParams.txt' & 
+nohup python2.7 gaBenchmarksStudy/benchmarks-uniform/benchmarks-gamodelF18.py -tournsize 14 -params 'gaBenchmarksStudy/benchmarks-uniform/gaParams.txt' & 
+nohup python2.7 gaBenchmarksStudy/benchmarks-uniform/benchmarks-gamodelF19.py -tournsize 14 -params 'gaBenchmarksStudy/benchmarks-uniform/gaParams.txt' & 
+nohup python2.7 gaBenchmarksStudy/benchmarks-uniform/benchmarks-gamodelF20.py -tournsize 14 -params 'gaBenchmarksStudy/benchmarks-uniform/gaParams.txt' & 
+wait
+nohup python2.7 gaBenchmarksStudy/benchmarks-uniform/benchmarks-gamodelF21.py -tournsize 14 -params 'gaBenchmarksStudy/benchmarks-uniform/gaParams.txt' & 
+nohup python2.7 gaBenchmarksStudy/benchmarks-uniform/benchmarks-gamodelF22.py -tournsize 14 -params 'gaBenchmarksStudy/benchmarks-uniform/gaParams.txt' & 
+nohup python2.7 gaBenchmarksStudy/benchmarks-uniform/benchmarks-gamodelF23.py -tournsize 14 -params 'gaBenchmarksStudy/benchmarks-uniform/gaParams.txt' & 
+nohup python2.7 gaBenchmarksStudy/benchmarks-uniform/benchmarks-gamodelF24.py -tournsize 14 -params 'gaBenchmarksStudy/benchmarks-uniform/gaParams.txt' & 
+wait
